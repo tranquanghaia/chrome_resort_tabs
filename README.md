ce0491d0179106deeb13b366cc424c571ddab7ae
tranquanghaia <tranquanghaic@gmail.com>
2024-11-18 14:09:09 +0700

    Init code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # chrome_resort_tabs
+
+Buy me a coffee: https://buymeacoffee.com/dngteam
---
+++ popup.js
@@ -0,0 +1,49 @@
+document.getElementById('groupTabs').addEventListener('click', async () => {
+    run()
+});
+async function run() {
+    const tabs = await chrome.tabs.query({ currentWindow: true });
+    const domainMap = {};
+
+
+    console.log(tabs);
+
+    tabs.forEach(tab => {
+        const url = new URL(tab.url);
+        let domain = url.hostname;
+        let tmp = tab.url.split('/');
+        if(tmp && tmp[3]){
+            domain+=tmp[3];
+        }
+        if (!domainMap[domain]) {
+            domainMap[domain] = [];
+        }
+       
+        domainMap[domain].push(tab.id);
+    });
+
+    console.log(domainMap);
+    // for (const domain in domainMap) {
+    //     const tabIds = domainMap[domain];
+    //     // Tạo một nhóm cho từng domain
+    //     await chrome.tabs.group({ tabIds });
+    // }
+    let sortedTabIds =[];
+     // Sắp xếp các tab theo thứ tự domain
+     for(let domain in domainMap){
+        let l = domainMap[domain];
+        for(let index in l){
+            sortedTabIds.push(l[index]);
+        }
+     }
+    //  const sortedTabIds = Object.values(domainMap).flat();
+    
+     // Đổi vị trí các tab
+     sortedTabIds.forEach((tabId, index) => {
+         chrome.tabs.move(tabId, { index });
+     });
+
+     console.log(sortedTabIds);
+}
+
+setTimeout(run,300)
